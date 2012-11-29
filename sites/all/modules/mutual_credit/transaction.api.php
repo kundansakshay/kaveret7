4711b36f5ff4864109ded0a643564b04d1d6a5c5
kundan <kundan@sakshay.in>
2012-11-29 17:30:10 +0530

    changes with ubercart

---
+++ sites/all/modules/mutual_credit/transaction.api.php
@@ -0,0 +1,246 @@
+<?php
+/**
+ * @file
+ * Formal description of transaction handling function and Entity controller functions
+ *
+ * N.B. transaction' can have 3 different meanings
+ *  a database transaction (not relevant to this document)
+ *  Fieldable entity with one or more '$items' each a in different currency
+ *  A cluster of those entities sharing the same serial number. The first is the volitional transaction and the rest are dependents
+ *
+ *
+ * Typical procedure for form processing might be
+ * form validation
+ *   //create the transaction object and save the extranneous fields
+ *   //use drupal_alter to add any dependent transactions
+ *   $transactions = array($transaction);
+ *   drupal_alter('transactions', $transactions);
+ *   transactions_insert($transactions, FALSE)
+ *     hook_transaction_validate
+ *     EntityController->insert($transactions, FALSE)
+ * end form validation
+ * form submission
+ *   //use drupal_alter to add any dependent transactions
+ *   $transactions = array($transaction);
+ *   drupal_alter('transactions', $transactions);
+ *   transactions_insert($transactions, TRUE);
+ *     hook_transaction_validate
+ *     EntityController->insert($transactions, TRUE)
+ *     field_attach_insert('transaction', $transaction);
+ *       hook_transactions_state
+ * end form submission
+ *
+ */
+
+/*
+ * Community Accounting API FOR MODULE DEVELOPERS
+ * WRAPPER FUNCTIONS
+ * These 3 wrapper functions around the following transaction controller API
+ * are mostly concerned with managing transactions as clusters sharing the same serial number
+ * Module developers should use these functions wherever possible.
+ */
+
+
+/*
+ * wrapper around Community Accounting API function transactions_load
+ * load a cluster of transactions sharing a serial number
+ * The first transaction will be the 'volitional' transaction and the rest are loaded into
+ * $transaction->children where the theme layer expects to find them
+ *
+ */
+transaction_load($serial);
+
+
+/*
+ * Entity API callback and wrapper around Community Accounting API function transactions_insert
+ * take one volitional transaction and processes it for rules, actions etc
+ * attempts to write them all
+ * returns them as an array
+ */
+try {
+  transaction_insert_new($transaction, $really = TRUE);
+}
+catch(exception $e){}
+
+ /*
+  * the following can be used for development
+  */
+transactions_delete($serials);
+
+
+
+/*
+ * TRANSACTION ENTITY CONTROLLER INTERFACE
+ * This is the actual api for transaction entity controllers
+ * Currently the module supports only one entity controller per drupal instance
+ * But it would be really powerful to support one entity controller  per currency
+ */
+
+/*
+ * Insert a cluster of validated transactions, which will receive the same serial number
+ * N.B. Contrib modules would normally call wrapper function transaction_insert_new()
+ * which fires the hook_transactions inserted
+ * $transactions is a flat array
+ * All $transactions will be given the same serial numbers
+ */
+try {
+  transactions_insert(&$transactions);
+}
+catch(exception $e){}
+
+/*
+ * The default entity controller supports 3 ways to undo
+ * Utter delete
+ * Change state to erased
+ * Create counter-transaction and set state of both to TRANSACTION_STATE_REVERSED
+ */
+try {
+  transactions_undo($transaction);
+}
+catch(exception $e){}
+
+
+/*
+ * very important to use this one for any state change,
+ * note that there is a hook that goes with it,
+ * hook_transaction_state($clusters, $new_state)
+ */
+try {
+  transactions_state($serials, $newstate);
+}
+catch(exception $e){}
+
+
+//retrieve an arbitrary transaction
+//actually default entity controller only does integer serial numbers
+$conditions = array('serial' => array('AB123', 'AB124'));
+//or
+$xids = array(234, 567);
+$transactions = transactions_load($xids, $conditions, $clearcache);
+
+
+/*
+ *
+ * Retrieves transaction summary data for a user in a given currency
+ *
+ * This data can also be obtained through various views fields, especially in the mcapi_index_views module
+ * $conditions are same as in drupal database api, each an array like ($fieldname, $value, $operator),
+ * where the fieldname is from mcapi_transactions table and the operator is optional.
+ * If there are no conditions passed then only transactions in a positive STATE are counted.
+ *
+ * Returns an array with the following keys
+ * - balance
+ * - gross_in
+ * - gross_out
+ * - count
+ */
+transaction_totals($uid, $currcode, $options);
+
+
+
+
+
+ /*
+ * Hooks provided by entity API module for this the transaction entity.
+ * THESE ARE PLACEHOLDERS, since the transaction entity does things quite differently
+ * to what the entity API module expects and it hasn't been coded yet
+ * constructed from template http://drupal.org/node/999936
+ */
+
+/**
+ * Acts on transactions being loaded from the database.
+ *
+ * This hook is invoked during transaction loading, which is handled by
+ * entity_load(), via the EntityCRUDController.
+ *
+ * @param array $transactions
+ *   An array of transaction entities being loaded, keyed by id.
+ *
+ * @see hook_entity_load()
+ */
+function hook_transaction_load(array $transactions) {
+  $result = db_query('SELECT pid, foo FROM {mytable} WHERE pid IN(:ids)', array(':ids' => array_keys($entities)));
+  foreach ($result as $record) {
+    $entities[$record->pid]->foo = $record->foo;
+  }
+}
+
+
+/**
+ * Act on a transaction that is being assembled before rendering.
+ *
+ * @param $transaction
+ *   The transaction entity.
+ * @param $view_mode
+ *   The view mode the transaction is rendered in.
+ * @param $langcode
+ *   The language code used for rendering.
+ *
+ * The module may add elements to $transaction->content prior to rendering. The
+ * structure of $transaction->content is a renderable array as expected by
+ * drupal_render().
+ *
+ * @see hook_entity_prepare_view()
+ * @see hook_entity_view()
+ */
+function hook_transaction_view($transaction, $view_mode, $langcode) {
+  $transaction->content['my_additional_field'] = array(
+    '#markup' => $additional_field,
+    '#weight' => 10,
+    '#theme' => 'mymodule_my_additional_field',
+  );
+}
+
+/**
+ * Alter the results of entity_view() for transactions.
+ *
+ * @param $build
+ *   A renderable array representing the transaction content.
+ *
+ * This hook is called after the content has been assembled in a structured
+ * array and may be used for doing processing which requires that the complete
+ * transaction content structure has been built.
+ *
+ * If the module wishes to act on the rendered HTML of the transaction rather than
+ * the structured content array, it may use this hook to add a #post_render
+ * callback. Alternatively, it could also implement hook_preprocess_transaction().
+ * See drupal_render() and theme() documentation respectively for details.
+ *
+ * @see hook_entity_view_alter()
+ */
+function hook_transaction_view_alter($build) {
+  if ($build['#view_mode'] == 'full' && isset($build['an_additional_field'])) {
+    // Change its weight.
+    $build['an_additional_field']['#weight'] = -10;
+
+    // Add a #post_render callback to act on the rendered HTML of the entity.
+    $build['#post_render'][] = 'my_module_post_render';
+  }
+}
+
+/**
+ * Define default transaction configurations.
+ *
+ * @return
+ *   An array of default transactions, keyed by machine names.
+ *
+ * @see hook_default_transaction_alter()
+ */
+function hook_default_transaction() {
+  $defaults['main'] = entity_create('transaction', array(
+    // …
+  ));
+  return $defaults;
+}
+
+/**
+ * Alter default transaction configurations.
+ *
+ * @param array $defaults
+ *   An array of default transactions, keyed by machine names.
+ *
+ * @see hook_default_transaction()
+ */
+function hook_default_transaction_alter(array &$defaults) {
+  $defaults['main']->name = 'custom name';
+}
---
+++ sites/all/modules/mutual_credit/transaction.tpl.php
@@ -0,0 +1,66 @@
+<?php
+// $Id: node-exchange.tpl.php,v 1.1.2.3 2010/12/06 13:19:46 hutch Exp $
+/*
+ * NOTE:
+ *
+ * Each currency can have its own transaction template
+ * Simply rename this file and rename it thus in your theme directory:
+ * transaction__X.tpl.php where X is the currency id
+ *
+ * see template_preprocess_transaction() for details
+ *
+ * $transaction     //entity object
+ * $view_mode       // either 'sentences' or 'certificate'
+ * $type            //
+ * $state           // 1 = pending, 0 = completed, -1 = erased
+ * $recorded        // date formatted using drupal's 'medium' date format
+ * $payer           // name linked to payer profile
+ * $payee           // name linked to payee profile
+ * $worth          // a comma separated list of formatted transaction values (in different currencies)
+ * $children        //an array of other transactions with the same serial number
+ *
+ * need to do some more work on the icon size
+ * for now, you might want to include your own large-size graphic instead
+ * using $transaction->quantity
+ * tip: $currency = currency_load($transaction->currcode);
+ *
+ * This template doesn't use normal template syntax because it's based on a sentence structure and needs to retain a coherent translatable string
+ * It is rather complex because of the need for translation
+ */
+$replacements = array(
+  '@recorded' => $recorded,
+  '!payer' => $payer,
+  '!payee' => $payee,
+  '!worth' => '<span class = "quantity">'. $worth .'</span>',
+);
+if ($view_mode == 'certificate') {
+  print render($pending_signatures); //floating the right, by default
+  $certificate_string = t(
+    'On @recorded<br />!payer <strong>paid</strong> !payee</br />the sum of !worth',
+    $replacements
+  );
+  $certificate_string = str_replace('<br/>', '<br /><br />', $certificate_string);
+}
+?>
+<div class = "<?php print $classes; ?>">
+  <?php if ($view_mode == 'certificate') : ?>
+    <?php print $certificate_string; ?>
+    <?php print render($additional); ?>
+  <?php elseif ($view_mode == 'sentences') :
+    if ($state > 0) {//transaction states > 0 are 'counted'
+      print t("On @recorded, !payer gave !payee !worth", $replacements);
+    }
+    else {//this is most likely a 'pending' transaction which is state -1
+      print t("On @recorded, !payer will give !payee !worth", $replacements);
+    }
+  endif; ?>
+
+  <?php if (isset($dependents)) : // all the remaining transactions are already rendered as sentences ?>
+  <div id="dependent-transactions">
+    <h3><?php print t('Dependent transactions'); ?></h3>
+    <?php print render ($dependents); ?>
+  </div>
+  <?php endif; ?>
+
+</div><!-- end transaction-->
+
